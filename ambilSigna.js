f9ea49d50cc25d44e9aba7165b762a195ad8cc92
Edy Cole <edycoleee@gmail.com>
2021-09-19 10:56:10 +0700

    error1

--- ambilSigna.js
+++ ambilSigna.js
@@ -4,23 +4,7 @@ const crypto = require("crypto");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const creatHeader = () => {
-  const consId = process.env.CONSID;
-  const secret = process.env.SECRET;
-  const user_key = process.env.USER_KEY;
-  const tStamp = Math.floor(Date.now() / 1000);
-  const data = consId + "&" + tStamp;
 
-  const signature = Buffer.from(
-    crypto.createHmac("SHA256", secret).update(data).digest()
-  ).toString("base64");
-  return {
-    "X-cons-id": consId,
-    "X-timestamp": tStamp,
-    "X-signature": signature,
-    user_key,
-  };
-};
 
 const options = {
   host: "dvlp.bpjs-kesehatan.go.id",
@@ -38,7 +22,6 @@ const uri = `https://${options.host}/${options.service}/`;
 const aplicareuri = `https://${aplicareOptions.host}${aplicareOptions.port}/${aplicareOptions.service}/`;
 
 module.exports = {
-  creatHeader,
   uri,
   aplicareuri,
 };
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const axios = require("axios");
 const crypto = require("crypto");
+const dotenv = require("dotenv");
 const LZString = require("lz-string");
-
-const { uri, creatHeader } = require("./ambilSigna");
+const { uri } = require("./ambilSigna");
+dotenv.config();
 
 const app = express();
 app.listen(3000, () => console.log("listening at 3000"));
@@ -13,9 +14,25 @@ app.get("/poli/:nmpoli", async (request, response) => {
   const nmpoli = request.params.nmpoli;
   const api_url = `${uri}referensi/poli/${nmpoli}`;
   console.log(api_url);
+
+  const consId = process.env.CONSID;
+  const secret = process.env.SECRET;
+  const user_key = process.env.USER_KEY;
+  const tStamp = Math.floor(Date.now() / 1000);
+  const data = consId + "&" + tStamp;
+  console.log(data);
+  const signature = Buffer.from(
+    crypto.createHmac("SHA256", secret).update(data).digest()
+  ).toString("base64");
+
   axios
     .get(api_url, {
-      headers: creatHeader(),
+      headers: {
+        "X-cons-id": consId,
+        "X-timestamp": tStamp,
+        "X-signature": signature,
+        user_key,
+      },
     })
     .then((response) => {
       console.log("response: ", response.data.response);
@@ -32,30 +49,43 @@ app.get("/poli/:nmpoli", async (request, response) => {
         //   $encrypt_method = 'AES-256-CBC';
         const encrypt_method = "aes-256-cbc";
         //   global $consID, $secretKey, $stamp;
+        console.log(consId, secret, tStamp.toString());
+        const signature1 = consId + secret + tStamp.toString();
         //   $signature = $consID . $secretKey . $stamp;
         //   // hash
-        const headers = creatHeader();
-        const signature = headers["X-signature"];
-        const key = crypto.createHash("sha256").update(signature).digest();
+        console.log(signature1);
+        const key = crypto
+          .createHash("sha256")
+          .update(signature1, "utf-8")
+          .digest("hex");
         //   $key = hex2bin(hash('sha256', $signature));
         //     // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
         //   $iv = substr(hex2bin(hash('sha256', $signature)), 0, 16);
         console.log("key", key);
+        console.log("Key length (bits):", key.length * 8);
         const iv = crypto
           .createHash("sha256")
-          .update(signature, "utf-8")
+          .update(signature1, "utf-8")
           .digest("hex")
           .substring(0, 16);
         console.log("iv", iv);
+        console.log("IV length (bits):", iv.length * 8);
+
+        //const cipherText = Buffer.from(data, "base64");
+        //console.log("cipherText", cipherText);
+
+        //const hasil = Buffer.concat([cipher.update(cipherText), cipher.final()]).toString("base64");
+        //console.log("hasil",hasil);
         const buf = Buffer.from(data, "base64");
         console.log("buf", buf);
         const crypt = buf.toString("base64");
         console.log("crypt", crypt);
 
+        console.log("decryptor crypto", encrypt_method, key, iv);
         const decryptor = crypto.createDecipheriv(encrypt_method, key, iv);
         console.log("decryptor", decryptor);
 
-        let resultDecryptor = decryptdecryptor.update(crypt);
+        let resultDecryptor = decryptdecryptor.update(buf);
         resultDecryptor += decryptor.final();
         console.log("hasil1", resultDecryptor);
         //return resultDecryptor;
